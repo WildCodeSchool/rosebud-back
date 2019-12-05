99224f8165729c89aa115d20c4530a183713bab3
Adrien Nombalier <adri1n.apple@gmail.com>
2019-12-05 11:31:15 +0100

    add post but not fonctional

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const connection = require("./config.js");
 
 const bodyParser = require("body-parser");
@@ -26,6 +26,22 @@ app.get(`/api/v1/questionnaires/:id`, (req, res) => {
       }
     }
   );
+});
+
+app.post('/api/v1/questionnaires/:id/participations', (req, res) => {
+  const formData = req.body;
+
+  connection.query('INSERT INTO movie SET ?', formData, (err, results) => {
+
+    if (err) {
+      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+      console.log(err);
+      res.status(500).send("Erreur lors de la sauvegarde d'un film");
+    } else {
+      // Si tout s'est bien passé, on envoie un statut "ok".
+      res.sendStatus(200);
+    }
+  });
 });
 
 app.listen(port, err => {
