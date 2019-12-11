98901b8f077ec353f88ad6584f98ed0235927114
FPaurisse <paurissefrederic@gmail.com>
2019-12-11 10:07:23 +0100

    add eslint config

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
+
 const connection = mysql.createConnection({
   host: process.env.DB_HOST, // adresse du serveur
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = connection;
