6e36aae0dfb6d4f471bec910c70f3ee1c5d15b3a
FPaurisse <paurissefrederic@gmail.com>
2019-12-11 11:05:30 +0100

    refactor before PR

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST, // adresse du serveur
--- index.js
+++ index.js
@@ -1,112 +1,103 @@
-require("dotenv").config();
-const express = require("express");
-const mysql = require("mysql2");
+require('dotenv').config();
+const express = require('express');
+
 const app = express();
 const port = 3001;
-const connection = require("./config.js");
+const bodyParser = require('body-parser');
+const connection = require('./config.js');
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true
-  })
+    extended: true,
+  }),
 );
 
 // GET QUESTIONS BY QUESTIONNAIRE ID
-app.get(`/api/v1/questionnaires/:id/questions`, (req, res) => {
-  const idQuestionnaire = req.params.id;
-  connection.query(
-    "SELECT * FROM questions WHERE questionnaire_id = ?",
-    [idQuestionnaire],
-    (err, results) => {
-      if (err) {
-        res.status(500).send("Erreur lors de la récupération des questions");
-      } else {
-        res.json(results);
-      }
-    }
-  );
+app.get('/api/v1/questionnaires/:id/questions', async (req, res) => {
+  const questionnaireId = req.params.id;
+  try {
+    const results = await connection.query(
+      'SELECT * FROM questions WHERE questionnaire_id = ?',
+      [questionnaireId],
+    );
+    return res.json(results);
+  } catch (err) {
+    return res.status(500).send('Erreur lors de la récupération des questions');
+  }
 });
 
 // POST PARTICIPATION BY QUESTIONNAIRE ID
-app.post("/api/v1/questionnaires/:id/participations", (req, res) => {
-  const { dataParticipant, dataAnswers } = req.body;
-  const delayInMilliseconds = 3000;
-  const valuesAnswers = dataAnswers.reduce(
+app.post('/api/v1/questionnaires/:id/participations', async (req, res) => {
+  const { participant, answers } = req.body;
+
+  try {
+    await connection.query(
+      'INSERT INTO participants (firstname, lastname, city, status, age, email) VALUES (?,?,?,?,?,?);',
+      [
+        participant.firstName,
+        participant.lastName,
+        participant.city,
+        participant.status,
+        participant.age,
+        participant.email,
+      ],
+    );
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send('Erreur lors de la sauvegarde de la participation');
+  }
+
+  const valuesAnswers = answers.reduce(
     (acc, curr) => [
       ...acc,
       curr.comment,
       curr.question_id,
-      curr.participant_id
     ],
-    []
+    [],
   );
-  connection.query(
-    `INSERT INTO participants (firstname, lastname, city, status, age, email) VALUES (?,?,?,?,?,?);`,
-    [
-      dataParticipant.firstName,
-      dataParticipant.lastName,
-      dataParticipant.city,
-      dataParticipant.status,
-      dataParticipant.age,
-      dataParticipant.email
-    ],
-    (err, participantsResults) => {
-      if (err) {
-        console.log(err);
-        res
-          .status(500)
-          .send("Erreur lors de la sauvegarde de la participation");
-      } else {
-        setTimeout(function() {
-          connection.query(
-            `INSERT INTO answers (comment, question_id, participant_id) VALUES ${dataAnswers.map(
-              _ => "(?,?,?)"
-            )};`,
-            valuesAnswers,
-            (err, answersResults) => {
-              if (err) {
-                console.log(err);
-                res
-                  .status(500)
-                  .send("Erreur lors de la sauvegarde de la participation");
-              } else {
-                res.status(200).send("OK");
-              }
-            },
-            delayInMilliseconds
-          );
-        });
-      }
-    }
-  );
+
+  try {
+    await connection.query(
+      `INSERT INTO answers (comment, question_id, participant_id) VALUES ${answers.map(
+        () => '(?,?,?)',
+      )};`,
+      valuesAnswers,
+    );
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send('Erreur lors de la sauvegarde des réponses');
+  }
+
+  res.status(200).send('OK');
 });
 
 // GET ANSWERS BY QUESTIONNAIRE ID
-app.get(`/api/v1/questionnaires/:id/participations`, (req, res) => {
+app.get('/api/v1/questionnaires/:id/participations', async (req, res) => {
   const idQuestionnaire = req.params.id;
-  connection.query(
-    `SELECT qts.id as questionnaire_id, qs.id as question_id, qs.title as question, a.comment as answer FROM questionnaires AS qts 
+  try {
+    const results = await connection.query(
+      `SELECT qts.id as questionnaire_id, qs.id as question_id, qs.title as question, a.comment as answer FROM questionnaires AS qts 
     JOIN questions AS qs ON qs.questionnaire_id=qts.id 
     JOIN answers AS a ON a.question_id = qs.id
     WHERE qts.id= ? ORDER BY qs.id;`,
-    [idQuestionnaire],
-    (err, results) => {
-      if (err) {
-        res
-          .status(500)
-          .send("Erreur lors de la récupération des participations");
-      } else {
-        res.json(results);
-      }
-    }
-  );
+      [idQuestionnaire],
+    );
+    res.json(results);
+  } catch (err) {
+    res
+      .status(500)
+      .send('Erreur lors de la récupération des participations');
+  }
 });
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
-    throw new Error("Something bad happened...");
+    throw new Error('Something bad happened...');
   }
   console.log(`Server is listening on ${port}`);
 });
