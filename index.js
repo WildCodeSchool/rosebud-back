ff3c10e0e053cef5c752b758946eca5ee870dde4
Falcao1992 <eduardo.marselle@gmail.com>
2019-12-12 10:47:56 +0100

    fonctionnelle en attente de la base de donné

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const path = require('path');
+const multer = require('multer');
+
 const app = express();
 const port = 3001;
 
@@ -18,10 +21,11 @@ app.use(
 app.get('/api/v1/questionnaires/:id/questions', async (req, res) => {
   const questionnaireId = req.params.id;
   try {
-    const [results] = await app.get('db').query(
-      'SELECT * FROM questions WHERE questionnaire_id = ?',
-      [questionnaireId],
-    );
+    const [results] = await app
+      .get('db')
+      .query('SELECT * FROM questions WHERE questionnaire_id = ?', [
+        questionnaireId,
+      ]);
     return res.json(results);
   } catch (err) {
     console.error(err);
@@ -34,17 +38,19 @@ app.post('/api/v1/questionnaires/:id/participations', async (req, res) => {
   const { participant, answers } = req.body;
 
   try {
-    await app.get('db').query(
-      'INSERT INTO participants (firstname, lastname, city, status, age, email) VALUES (?,?,?,?,?,?);',
-      [
-        participant.firstName,
-        participant.lastName,
-        participant.city,
-        participant.status,
-        participant.age,
-        participant.email,
-      ],
-    );
+    await app
+      .get('db')
+      .query(
+        'INSERT INTO participants (firstname, lastname, city, status, age, email) VALUES (?,?,?,?,?,?);',
+        [
+          participant.firstName,
+          participant.lastName,
+          participant.city,
+          participant.status,
+          participant.age,
+          participant.email,
+        ],
+      );
   } catch (err) {
     console.error(err);
     return res
@@ -53,26 +59,22 @@ app.post('/api/v1/questionnaires/:id/participations', async (req, res) => {
   }
 
   const valuesAnswers = answers.reduce(
-    (acc, curr) => [
-      ...acc,
-      curr.comment,
-      curr.question_id,
-    ],
+    (acc, curr) => [...acc, curr.comment, curr.question_id],
     [],
   );
 
   try {
-    await app.get('db').query(
-      `INSERT INTO answers (comment, question_id, participant_id) VALUES ${answers.map(
-        () => '(?,?,?)',
-      )};`,
-      valuesAnswers,
-    );
+    await app
+      .get('db')
+      .query(
+        `INSERT INTO answers (comment, question_id, participant_id) VALUES ${answers.map(
+          () => '(?,?,?)',
+        )};`,
+        valuesAnswers,
+      );
   } catch (err) {
     console.error(err);
-    return res
-      .status(500)
-      .send('Erreur lors de la sauvegarde des réponses');
+    return res.status(500).send('Erreur lors de la sauvegarde des réponses');
   }
   res.status(200).send('OK');
 });
@@ -90,10 +92,82 @@ app.get('/api/v1/questionnaires/:id/participations', async (req, res) => {
     );
     res.json(results);
   } catch (err) {
-    res
-      .status(500)
-      .send('Erreur lors de la récupération des participations');
+    res.status(500).send('Erreur lors de la récupération des participations');
   }
+});
+
+// UPLOAD IMAGE
+
+// Set Storage Engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`,
+    );
+  },
+});
+
+// Init Upload
+const upload = multer({
+  storage,
+  limits: { fileSize: 1000000 },
+  fileFilter(req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single('myImage');
+
+// EJS
+app.set('view engine', 'ejs');
+
+// Check File Type
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  }
+  cb('Erreur: Images Seulement !');
+}
+
+// Public Folder
+app.use(express.static('./public'));
+
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.send({ msg: err });
+    } else if (req.file == undefined) {
+      res.send({ msg: 'Pas De Fichier Selectioné' });
+    } else {
+      console.log('file received');
+      console.log(req.file);
+
+      const sql = `INSERT INTO \`file\`(\`name\`, \`type\`, \`path\`) VALUES ('${
+        req.file.filename
+      }', '${
+        req.file.mimetype
+      }', '${
+        req.file.path
+      }')`;
+      msg = 'Successfully! uploaded';
+      res.send({
+        msg:
+            `Fichier Téléchargé Avec Succès ! voici le chemin de votre image ${
+              req.file.path
+            }${req.file.size / 10000
+            }Mo`,
+        file: `uploads/${req.file.filename}`,
+        path: req.file.path,
+      });
+    }
+  });
 });
 
 app.listen(port, (err) => {
