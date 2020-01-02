0fb7d3b53a821e02f2623863cb5876772c6e706d
FPaurisse <paurissefrederic@gmail.com>
2020-01-02 09:38:00 +0100

    dev

--- index.js
+++ index.js
@@ -39,9 +39,10 @@ app.post('/api/v1/questionnaires/:QuestionnaireId/participations', upload.any(),
     email,
     QuestionnaireId,
   });
+  console.log(req.files);
   const answers = [];
   for (let i = 0; i < questionsLength; i += 1) {
-    const { path } = req.files[i];
+    const path = req.files[i];
     const {
       [`answerComment${i}`]: comment, [`questionId${i}`]: QuestionId,
     } = req.body;
