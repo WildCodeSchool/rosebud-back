776eb8ad0180d8d4fcc0e37351350792f1fd5200
FPaurisse <paurissefrederic@gmail.com>
2020-02-05 15:06:27 +0100

    edit migration addColumn

--- migrations/20200205133828-add-isPrivate.js
+++ migrations/20200205133828-add-isPrivate.js
@@ -1,51 +1,8 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('Questionnaires', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER,
-    },
-    title: {
-      allowNull: false,
-      type: Sequelize.STRING,
-    },
-    participationText: {
-      allowNull: true,
-      type: Sequelize.STRING,
-    },
-    presentationText: {
-      allowNull: true,
-      type: Sequelize.STRING,
-    },
-    UserId: {
-      allowNull: false,
-      type: Sequelize.INTEGER,
-      references: {
-        model: 'Users',
-        key: 'id',
-      },
-    },
-    isOnline: {
-      allowNull: false,
-      type: Sequelize.BOOLEAN,
-      defaultValue: false,
-    },
-    isPrivate: {
-      allowNull: false,
-      type: Sequelize.BOOLEAN,
-      defaultValue: false,
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
-    },
+  up: (queryInterface, Sequelize) => queryInterface.addColumn('Questionnaires', 'isPrivate', {
+    allowNull: false,
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
   }),
-  down: (queryInterface, Sequelize) => queryInterface.dropTable('Questionnaires'),
+  down: (queryInterface, Sequelize) => queryInterface.removeColumn('Questionnaires', 'isPrivate'),
 };
